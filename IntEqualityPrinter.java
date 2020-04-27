d87810ae50bbed8b827ee4a96dc4d679ba4d42fa
bryantlabs <ronroberts@callronroberts.com>
2020-04-26 21:49:40 -0500

    IntEqualityPrinter.java Exercise

--- IntEqualityPrinter.java
+++ IntEqualityPrinter.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class IntEqualityPrinter {
-}
+
+    public static void printEqual(int numberOne, int numberTwo, int numberThree) {
+            if ((numberOne < 0) || (numberTwo < 0) || (numberThree < 0)) {
+            System.out.println("Invalid Value");
+        } else if
+             ((numberOne == numberTwo) && (numberOne == numberThree)) {
+                System.out.println("All numbers are equal");
+            } else {
+                if ((numberOne != numberTwo) && (numberTwo != numberThree) &&
+                        (numberOne != numberThree)) {
+                    System.out.println("All numbers are different");
+                } else {
+                    System.out.println("Neither all are equal or different");
+                }
+            }
+        }
+    }
+

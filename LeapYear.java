46eefdb91b894435a647d8c68d77aab0fd373ab7
bryantlabs <ronroberts@callronroberts.com>
2020-04-26 21:52:19 -0500

    LeapYear.java Exercise

--- LeapYear.java
+++ LeapYear.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class LeapYear {
+
+    public static boolean isLeapYear(int year){
+        if ((year < 1) || (year > 9999)){
+            return false;
+        }else {
+            if (((year % 4 == 0) && (year % 100 == 0)) &&
+            (year % 400 == 0)){
+                return true;
+            }else if (((year % 4 == 0) && (year % 100 != 0))){
+                return true;
+            }
+        }
+        return false;
+    }
 }

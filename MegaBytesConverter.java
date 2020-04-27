b679704de15b7f7ed66ba26a86857c8ffbfa8a0d
bryantlabs <ronroberts@callronroberts.com>
2020-04-26 21:54:20 -0500

    MegaBytesConverter.java Exercise

--- MegaBytesConverter.java
+++ MegaBytesConverter.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class MegaBytesConverter {
+
+ public static void printMegaBytesAndKiloBytes(int kilobytes){
+
+     if (kilobytes < 0){
+         System.out.println("Invalid Value");
+     }else {
+         int divisor = 1024;
+         int dividend = kilobytes;
+/* The variable dividend here is noted as redundant by I choose to use it because
+   it helps me read the code easier.   */
+         int quotient = dividend / divisor;
+         int remainder = dividend % divisor;
+
+         System.out.println(dividend + " KB = " + quotient + " MB " +"and " + remainder + " KB");
+
+     }
+ }
+
 }

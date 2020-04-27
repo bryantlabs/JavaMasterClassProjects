224caa989c4106fb7bb798bf1e1d98e3f330a94f
bryantlabs <ronroberts@callronroberts.com>
2020-04-26 21:43:29 -0500

    DecimalComparator.java Exercise

--- DecimalComparator.java
+++ DecimalComparator.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class DecimalComparator {
+
+    public static boolean areEqualByThreeDecimalPlaces(double numberOne, double numberTwo){
+
+                int myNewNumberOne = (int)(numberOne *1000);
+                int myNewNumberTwo = (int)(numberTwo *1000);
+
+                if (myNewNumberOne == myNewNumberTwo){
+                    return true;
+                }
+                return false;
+    }
+
 }

f833243e9b4df7af30bc07b269fca7a838c0d071
bryantlabs <ronroberts@callronroberts.com>
2020-04-26 21:41:57 -0500

    BarkingDog.java Exercise

--- BarkingDog.java
+++ BarkingDog.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class BarkingDog {
+
+    public static boolean  shouldWakeUp(boolean barking, int hourOfDay){
+
+        if ((hourOfDay < 0) || (hourOfDay > 23)){
+            return false;
+        } else {
+            if ((barking) && (hourOfDay < 8) || (hourOfDay > 22)){
+                /*The above line could be simplified but I choose to keep it
+                in its present form so I can read it easier.
+                If you like, you could simplify it to the following:
+                return (barking) && (hourOfDay < 8) || (hourOfDay > 22) and
+                remove the last return false;
+                 */
+                return true;
+            }
+            return false;
+        }
+
+    }
 }

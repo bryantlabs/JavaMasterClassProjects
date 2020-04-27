7872f2dce6be94bab0a986f610ee9314d4f629b3
bryantlabs <ronroberts@callronroberts.com>
2020-04-26 21:46:34 -0500

    EqualSum.java

--- EqualSum.java
+++ EqualSum.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class EqualSum {
+
+    public static boolean hasEqualSum(int numberOne, int numberTwo, int numberThree) {
+
+        if (numberOne + numberTwo == numberThree) {
+            return true;
+
+//     could be shortened to  return (numberOne + numberTwo == numberThree);
+        }
+        return false;
+    }
 }

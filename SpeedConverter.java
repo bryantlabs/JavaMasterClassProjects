6fa35f0d79ff29399f82411c53efee9966592387
bryantlabs <ronroberts@callronroberts.com>
2020-04-26 22:07:43 -0500

    SpeedConverter.java Exercise

--- SpeedConverter.java
+++ SpeedConverter.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class SpeedConverter {
+
+    public static long toMilesPerHour(double kilometersPerHour){
+
+        if (kilometersPerHour < 0){
+            return -1;
+        }else {
+         return Math.round(kilometersPerHour / 1.609);
+         //Even though the Math.round() accepts either a float or double
+            // we can initialize a variable in our main method of type long
+            // to receive the returned Math.round value.
+        }
+    }
+
+    public static void  printConversion(double kilometersPerHour){
+        if (kilometersPerHour < 0){
+            System.out.println("Invalid Value");
+        }else{
+            long totalMiles = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h = " +
+                    totalMiles + " mi/h");
+        }
+    }
+
+
 }

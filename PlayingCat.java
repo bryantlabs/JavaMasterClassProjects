823f74341044458c31535f6f1ac4cff5b5c3046d
bryantlabs <ronroberts@callronroberts.com>
2020-04-26 22:00:38 -0500

    PlayingCat.java Exercise

--- PlayingCat.java
+++ PlayingCat.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class PlayingCat {
+
+    public static boolean isCatPlaying(boolean summer, int temperature){
+
+        if ((summer == true) && (temperature >= 25) && (temperature <= 45)){
+            return true;
+        }
+        else if ((summer == false) && (temperature >= 25) && (temperature <= 35)){
+            return true;
+        }
+            return false;
+    }
 }

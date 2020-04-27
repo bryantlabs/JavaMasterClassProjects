91ed7a747927449ec650991338cedd751538ea75
bryantlabs <ronroberts@callronroberts.com>
2020-04-26 22:09:23 -0500

    TeenNumberChecker.java

--- TeenNumberChecker.java
+++ TeenNumberChecker.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
 public class TeenNumberChecker {
+
+    public static boolean hasTeen(int teenOne, int teenTwo, int teenThree) {
+
+        if ((teenOne < 13 || teenOne > 19) && (teenTwo < 13 || teenTwo > 19) &&
+                (teenThree < 13 || teenThree > 19)) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isTeen(int teen) {
+        if ((teen >= 13 && teen <= 19)) {
+            return true;
+        }
+        return false;
+    }
 }
+
+//Below is an altenative way of solving the exercise
+
+//public class TeenNumberChecker {
+
+//    public static boolean hasTeen(int teenOne, int teenTwo, int teenThree){
+//        if ((teenOne >= 13 && teenOne <= 19) || (teenTwo >= 13 && teenTwo <= 19) ||
+//                (teenThree >= 13 && teenThree <= 19)){
+//            return true;
+//        }
+//        return false;
+//    }
+
+//    public static boolean isTeen(int teen) {
+//      if ((teen >= 13 && teen <= 19)) {
+//            return true;
+//        }
+//        return false;
+//
+//
+//    }

fb001acd78ada9657ad62aa5f25422b60667d9eb
bryantlabs <ronroberts@callronroberts.com>
2020-05-02 20:17:53 -0500

    GreatestCommonDivisor.java Exercise

--- GreatestCommonDivisor.java
+++ GreatestCommonDivisor.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class GreatestCommonDivisor {
+
+    public static int getGreatestCommonDivisor(int numberOne, int numberTwo){
+        int gcd = 0;
+        if ((numberOne < 10) || (numberTwo < 10)){
+            return -1;
+        } else {
+
+            for (int i = 2; i <= numberOne || i <= numberTwo; i++){
+                if ((numberOne % i == 0) && (numberTwo % i == 0)){
+                    gcd = i;
+                }
+            }
+        }
+        return gcd;
+    }
 }

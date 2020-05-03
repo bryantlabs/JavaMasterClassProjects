2bd3d65f5c3ff731fd8edff671c5dd18b626af79
bryantlabs <ronroberts@callronroberts.com>
2020-05-02 20:10:22 -0500

    SharedDigit.java

--- SharedDigit.java
+++ SharedDigit.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class SharedDigit {
-}
+
+    public static boolean hasSharedDigit(int oneNumber, int twoNumber) {
+
+        int oneLastDigit = 0;
+        int twoLastDigit = 0;
+
+        if ((oneNumber < 10 || oneNumber > 99) || (twoNumber < 10 || twoNumber > 99)) {
+            return false;
+        } else
+            while ((oneNumber > 10) && (twoNumber > 10)) {
+
+                oneLastDigit = oneNumber % 10;
+                oneNumber = oneNumber / 10;
+
+                twoLastDigit = twoNumber % 10;
+                twoNumber = twoNumber / 10;
+
+                if ((oneLastDigit == twoLastDigit) || (oneLastDigit == twoNumber) ||
+                        (oneNumber == twoLastDigit) || (oneNumber == twoNumber)) {
+                    return true;
+                }
+            }
+        return false;
+        }
+
+    }
+
+
+

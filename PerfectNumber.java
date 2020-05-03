66bade2be06ed41abbb89b6eb883963dea3ea058
bryantlabs <ronroberts@callronroberts.com>
2020-05-02 20:12:23 -0500

    PerfectNumber.java

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class PerfectNumber {
+    public static boolean isPerfectNumber(int number){
+        if (number < 1){
+            return false;
+        } else {
+            int perfectSum = 0;
+            for(int i = 1; i < number; i++){
+                if (number % i == 0){
+                    System.out.println("factors for number = " + i);
+                    perfectSum = perfectSum + i;
+                    System.out.println(perfectSum);
+
+                }
+                if (perfectSum == number){
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+
 }

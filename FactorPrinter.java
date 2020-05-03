87f0aaa57df32a361d2518849773dbd938ad98b4
bryantlabs <ronroberts@callronroberts.com>
2020-05-02 20:21:25 -0500

    FactorPrinter.java Exercise

--- FactorPrinter.java
+++ FactorPrinter.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class FactorPrinter {
+
+    public static void printFactors(int number) {
+
+        if (number < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            for (int i = 1; i <= number; i++) {
+                if (number % i == 0) {
+                    int factor = i;      // See note below;
+                    System.out.println(factor);
+                }
+            }
+        }
+    }
 }
+
+/* IntelliJ tells me local variable factor is redundant
+   but I think it helps me read the code better.
+    I wanted anyone reading the code to be able to see that
+    I was calculating for factors of a number and that the
+    for() was using i as potential factors. */

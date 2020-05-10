60886f68a8c7496d6cde073c0a158403081211ea
bryantlabs <ronroberts@callronroberts.com>
2020-05-10 13:21:24 -0500

    AdditionalAsterickPattern.java Exercise

--- AdditionalAsterickPattern.java
+++ AdditionalAsterickPattern.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+//An additional asterick pattern using only for()'s;
 
 public class AdditionalAsterickPattern {
+
+    public static void  additionalAsterick(int number){
+
+        for (int i = 1; i <= number; i++){
+            for (int j= 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for (int i = (number - 1); i > 0; i--){
+            for (int j= 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
 }
